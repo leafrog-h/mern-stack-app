403325b95dbcc2afd243a5fcd9c0d3f013408cd2
leafrog-h <zacharyhzh4@gmail.com>
2019-07-01 21:36:34 +0800

    ''

--- server/routes/api/edit_exercise.js
+++ server/routes/api/edit_exercise.js
@@ -2,14 +2,28 @@ const express = require("express");
 const router = express.router();
 const ExerCise = require("../../models/exercise_schema");
 
-router.post("/:exerciseKey", async (req, res) => {
+router.get("/:exerciseKey", async (req, res) => {
   try {
     const exerciseKey = req.params.exerciseKey;
     const exerciseToEdit = await ExerCise.find({ exerciseKey });
     res.status(201).json(exerciseToEdit);
   } catch (err) {
-    res.status(500).json({ mes: "fail to edit an exercise" });
+    res.status(500).json({ meg: "fail to edit an exercise" });
   }
 });
 
+router.patch("/:exerciseKey", async (req, res) => {
+  try {
+    const { exercise, description, duration } = req.body;
+    const exerciseKey = req.params.exerciseKey;
+    const updatedExercise = await ExerCise.update(
+      { exerciseKey },
+      { $set: { exercise, description, duration } }
+    );
+    res.status(201).json(updatedExercise);
+  } catch (error) {
+    console.err(error);
+    res.json(500).json({ msg: "fail to edit exercise" });
+  }
+});
 module.exports = router;
